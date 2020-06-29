a1724e643e848ddfeda6f711eb719b07395119ab
Andi Drebes <andi@drebesium.org>
2020-06-29 22:19:59 +0200

    Add function detecting matrix-vector products
    Add a function `teckyl::patterns::isMatvecComprehension` that checks
    if a tensor expression represents a matrix-vector product, i.e., has
    the pattern:
    
      C(i) +=! A(i, k) * B(k)
    
    or
    
      C(i) +=! B(k) * A(i, k)

--- teckyl/patterns.h
+++ teckyl/patterns.h
@@ -6,6 +6,94 @@
 namespace teckyl {
 namespace pattern {
 
+// Checks if a comprehension is a matrix-vector product, i.e., if it
+// has the pattern
+//
+//   C(i) +=! A(i, k) * B(k) or
+//   C(i) +=! B(k) * A(i, k)
+//
+// Returns true if the pattern matches, otherwise false. If
+// inverse_operands is non-NULL, its value will be set to false if the
+// first pattern matches or true if the second pattern matches. If no
+// pattern matches, inverse_operands is left untouched.
+static inline bool isMatvecComprehension(const lang::Comprehension &c,
+                                         size_t (*canonical_order)[2] = NULL) {
+  lang::ListView<lang::Ident> lhsIdents = c.indices();
+
+  // Ensure this is a sum of products
+  if (c.assignment()->kind() != lang::TK_PLUS_EQ_B || c.rhs()->kind() != '*')
+    return false;
+
+  // Ensure that the output is a vector
+  if (lhsIdents.size() != 1)
+    return false;
+
+  // Ensure that there are exactly two operands to the multiplication
+  if (c.rhs()->trees().size() != 2)
+    return false;
+
+  // Ensure that the source operands are indexed tensors
+  if (c.rhs()->tree(0)->kind() != lang::TK_ACCESS ||
+      c.rhs()->tree(1)->kind() != lang::TK_ACCESS) {
+    return false;
+  }
+
+  std::vector<lang::Access> accesses = {lang::Access(c.rhs()->tree(0)),
+                                        lang::Access(c.rhs()->tree(1))};
+
+  bool inv;
+
+  if (accesses[0].arguments().size() == 2 &&
+      accesses[1].arguments().size() == 1) {
+    // Matrix is the first operand, vector is the second operand
+    inv = false;
+  } else if (accesses[0].arguments().size() == 1 &&
+             accesses[1].arguments().size() == 2) {
+    // Vector is the first operand, matrix is the second operand
+    inv = true;
+    accesses = {accesses[1], accesses[0]};
+  } else {
+    // Operands are not exactly one matrix and one vector
+    return false;
+  }
+
+  // Ensure that the output operand is not used as an input
+  if (accesses[0].name().name() == c.ident().name() ||
+      accesses[1].name().name() == c.ident().name()) {
+    return false;
+  }
+
+  // Check that the matrix and vector are indexed directly by identifiers
+  if (accesses[0].arguments()[0]->kind() != lang::TK_IDENT ||
+      accesses[0].arguments()[1]->kind() != lang::TK_IDENT ||
+      accesses[1].arguments()[0]->kind() != lang::TK_IDENT) {
+    return false;
+  }
+
+  lang::Ident matrixIdents[2] = {lang::Ident(accesses[0].arguments()[0]),
+                                 lang::Ident(accesses[0].arguments()[1])};
+  lang::Ident vectorIdent = lang::Ident(accesses[1].arguments()[0]);
+
+  // Ensure that the iterator for the output is used to index the
+  // first dimension of the input matrix and that the iterator for the
+  // second dimension of the input matrix is used to iterate over the
+  // input vector
+  bool ret = lhsIdents[0].name() == matrixIdents[0].name() &&
+             matrixIdents[1].name() == vectorIdent.name();
+
+  if (ret && canonical_order) {
+    if (!inv) {
+      (*canonical_order)[0] = 0;
+      (*canonical_order)[1] = 1;
+    } else {
+      (*canonical_order)[0] = 1;
+      (*canonical_order)[1] = 0;
+    }
+  }
+
+  return ret;
+}
+
 // Checks if a comprehension is a matrix multiplication, i.e., if it
 // has the pattern
 //
